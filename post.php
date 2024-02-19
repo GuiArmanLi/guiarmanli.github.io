8a0d14c9d289166ba80f77b9d965eeb2a346d882
Guilherme <guilhermesantanapessa12@gmail.com>
2024-02-19 12:34:11 -0300

    Conecao local com PostgreSQL com composer e doctrine

--- post.php
+++ post.php
@@ -1,24 +1,34 @@
 <?php
-$servername = getenv("SERVERNAME");
+require_once 'vendor/autoload.php';
+use Doctrine\DBAL\DriverManager;
+
+$host = getenv("HOST"); #Ainda local
+$port = getenv("PORT");
+$dbname = getenv("DBNAME");
 $username = getenv("USERNAME");
 $password = getenv("PASSWORD");
-$dbname = getenv("DBNAME");
+$tableName = getenv("TABLE_NAME");
 
-$connection = new mysqli($servername, $username, $password, $dbname);
+$connectionParams = [
+    'dbname' => "site",
+    'user' => "postgres",
+    'password' => "admin",
+    'host' => "localhost",
+    'driver' => 'pdo_pgsql',
+];
 
-if ($connection->connect_error) {
-    die("Erro: " . $connection->connect_error);
-}
+$connection = DriverManager::getConnection($connectionParams);
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-$sql = "INSERT INTO usuarios (nome, email) VALUES ('$name', '$email')";
-
-if ($connection->query($sql)) {
-    echo "Sucesso!";
-} else {
-    echo "Erro: " . $sql . "\t" . $connection->error;
+$query = "INSERT INTO users_test (name, email) VALUES ('$name', '$email')";
+try {
+    $connection->executeQuery($query);
+    
+} catch (Exception $error) {
+    echo "Erro: " . $error->getMessage();
+    exit;
 }
 
-$connection->close();
+echo "Enviado";
